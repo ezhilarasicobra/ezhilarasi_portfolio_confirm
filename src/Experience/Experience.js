6c786ec73439d7aa0dfd944799dbba242a3107b0
Ezhilarasi <tezzhilarasi@gmail.com>
2021-12-15 16:47:49 +0530

    upadted experience

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -26,7 +26,7 @@ function Experience() {
     </div>
 
     <div className="tl-content">
-      <h1>Lorem ipsum dolor sit</h1>
+      <h1>Cognizant Technology Solution- Programmer Analyst</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
     </div>
 
